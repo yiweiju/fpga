a0e5903b8253a69ddb5edf53e198d74fe18e7633
YIDAN XU <917841885@qq.com>
2024-12-06 14:03:12 +0800

    Add files via upload

---
+++ final/prj/output_files/Top_VGA_Game_Snake.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 23.1std.1 Build 993 05/14/2024 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE10F17) Path("C:/Users/XU YIDAN/Desktop/fpga/snake/prj/output_files/") File("Top_VGA_Game_Snake.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ final/prj/output_files/Top_VGA_Game_Snake.tcl
@@ -0,0 +1,61 @@
+# Copyright (C) 2018  Intel Corporation. All rights reserved.
+# Your use of Intel Corporation's design tools, logic functions 
+# and other software and tools, and its AMPP partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Intel Program License 
+# Subscription Agreement, the Intel Quartus Prime License Agreement,
+# the Intel FPGA IP License Agreement, or other applicable license
+# agreement, including, without limitation, that your use is for
+# the sole purpose of programming logic devices manufactured by
+# Intel and sold by Intel or its authorized distributors.  Please
+# refer to the applicable agreement for further details.
+
+# Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Standard Edition
+# File: E:\prj_practice\10 snake_vga\prj\output_files\Top_VGA_Game_Snake.tcl
+# Generated on: Thu Nov 28 13:48:47 2024
+
+package require ::quartus::project
+
+set_location_assignment PIN_E1 -to sys_clk_50
+set_location_assignment PIN_M2 -to key[0]
+set_location_assignment PIN_M1 -to key[1]
+set_location_assignment PIN_E15 -to key[2]
+set_location_assignment PIN_E16 -to key[3]
+set_location_assignment PIN_M15 -to sys_rst_n
+set_location_assignment PIN_C2 -to vga_hs
+set_location_assignment PIN_D1 -to vga_vs
+
+set_location_assignment PIN_B4 -to vga_rgb[0]
+set_location_assignment PIN_A2 -to vga_rgb[1]
+set_location_assignment PIN_B5 -to vga_rgb[2]
+set_location_assignment PIN_A6 -to vga_rgb[3]
+set_location_assignment PIN_B6 -to vga_rgb[4]
+set_location_assignment PIN_A5 -to vga_rgb[5]
+set_location_assignment PIN_F6 -to vga_rgb[6]
+set_location_assignment PIN_F7 -to vga_rgb[7]
+set_location_assignment PIN_A7 -to vga_rgb[8]
+set_location_assignment PIN_B7 -to vga_rgb[9]
+set_location_assignment PIN_E8 -to vga_rgb[10]
+set_location_assignment PIN_F8 -to vga_rgb[11]
+set_location_assignment PIN_A8 -to vga_rgb[12]
+set_location_assignment PIN_B8 -to vga_rgb[13]
+set_location_assignment PIN_E7 -to vga_rgb[15]
+set_location_assignment PIN_E6 -to vga_rgb[14]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
